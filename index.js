6c4cb9853e42bd05f03e93cf63c8859266e8e6db
krishnareddy756 <162585616+krishnareddy756@users.noreply.github.com>
2025-03-01 23:46:31 +0530

    miscellaneous

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
-const express = require('express');
-const app = express();
-
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
-
-const posts = [
-    { username: "john_doe", userAvatar: "/avatar1.jpg", imageUrl: "/post1.jpg", caption: "Beautiful day!", likes: 120 },
-    { username: "jane_doe", userAvatar: "/avatar2.jpg", imageUrl: "/post2.jpg", caption: "Enjoying my coffee ☕", likes: 200 }
-];
-
-app.get('/', (req, res) => {
-    res.render('index', { posts });
+const express=require('express');
+const app=express();
+const port=8080;
+app.get('/register',(req,res)=>{ 
+    res.send('standard GET REQUEST');
 });
-
-app.get('/login', (req, res) => {
-    res.render('login');
+app.post('/register',(req,res)=>{
+    res.send('standard POST REQUEST');
 });
-
-app.get('/register', (req, res) => {
-    res.render('register');
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
 });
-
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
