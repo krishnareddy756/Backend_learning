8a66e661dec13ee5f82c5230a0a888c5eb3c708e
krishnareddy756 <162585616+krishnareddy756@users.noreply.github.com>
2025-03-01 23:39:18 +0530

    EXPRESS JS

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const figlet=require('figlet');
-figlet("Hello World!!", function (err, data) {
-    if (err) {
-      console.log("Something went wrong...");
-      console.dir(err);
-      return;
-    }
-    console.log(data);
-  });
+const express=require('express');
+const app=express();
+const port=8080;
+app.listen(port,()=>{   console.log(`Server is running on port ${port}`);}  );
+// app.use((req,res)=>{
+//     console.log('message received'); 
+//     res.send('Hello World');
+// })
+app.get('/users/:id', (req, res) => {
+    const userId = req.params.id;
+    res.send(`User ID: ${userId}`);
+});
+app.get('/search', (req, res) => {
+    const query = req.query;
+    res.send(`Search Query: ${query}`);
+});
